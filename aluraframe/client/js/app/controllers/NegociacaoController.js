9af1f300140d9986c316b411707d7ee2ae00d49b
janaparadiso <janaparadiso@gmail.com>
2016-12-22 21:32:56 -0200

    aula 5

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,24 +6,29 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
     
     adiciona(event) {
         
         event.preventDefault();
-      
-        let data = new Date(
-            ...this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-            
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value  
-        );
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();   
+    }
+    
+    _criaNegociacao() {
         
-        console.log(negociacao);
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);    
+    }
+    
+    _limpaFormulario() {
+     
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();   
     }
 }
