4db74f7f374796af96793d70fc2d715e6f24fa45
janaparadiso <janaparadiso@gmail.com>
2016-12-22 21:31:49 -0200

    aula 4

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -2,7 +2,6 @@ class Negociacao {
     
     constructor(data, quantidade, valor) {
         
-        
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
@@ -15,14 +14,17 @@ class Negociacao {
     }
     
     get data() {
+        
         return new Date(this._data.getTime());
     }
     
     get quantidade() {
+        
         return this._quantidade;
     }
     
     get valor() {
+        
         return this._valor;
     }
 }
